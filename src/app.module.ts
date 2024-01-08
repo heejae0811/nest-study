440dbfc09cae778d46e5ed70cef9ab8d4b75b6cd
이희재 <hj.lee2@stickint.kr>
2024-01-08 17:03:08 +0900

    Modify 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
-import { TestController } from './test/test.controller';
-import { TestService } from './test/test.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController, TestController],
-  providers: [MoviesService, TestService],
+  imports: [MoviesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
